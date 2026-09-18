7007391c142b9e90cb59949d9dc5c8c0f0fe8594
SatyaSrinivas15 <satyasrinivaskotti@gmail.com>
2026-09-18 12:09:58 +0530

    Add files via upload

---
+++ ClassRoom_Tasks/ClassRoom_Task1_Question2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class ClassRoom_Task1_Question2 {
+    public static void main(String[] args) {
+
+        List<Integer> marks = Arrays.asList(45, 78, 32, 90, 65, 88, 40, 95);
+
+        List<Integer> updatedMarks = marks.stream()
+                .filter(m -> m >= 60)
+                .map(m -> m + 5)
+                .collect(Collectors.toList());
+
+        double average = updatedMarks.stream()
+                .mapToInt(m -> m)
+                .average()
+                .orElse(0.0);
+
+        List<Integer> descendingMarks = updatedMarks.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        System.out.println("Marks greater than or equal to 60:");
+        System.out.println(updatedMarks);
+
+        System.out.println("Average of updated marks: " + average);
+
+        System.out.println("Marks in descending order:");
+        System.out.println(descendingMarks);
+    }
+}
